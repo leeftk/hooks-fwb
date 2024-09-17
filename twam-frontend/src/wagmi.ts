dcd900d1c6c9d3027752242cd7240ad6264f14a6
Leandro Faria <lee.lara1219@gmail.com>
2024-09-16 21:41:39 -0400

    add frontend

---
+++ twam-frontend/src/wagmi.ts
@@ -0,0 +1,25 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+  anvil,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    anvil,
+    sepolia
+  ],
+  ssr: true,
+});
